ca190d3e3238efbb632d7506e3c34219cd568813
Arbab Qadeer <80912992+ArbabQadeer@users.noreply.github.com>
2023-04-07 04:28:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+  function openNav() {
+          document.getElementById("sidebar").style.left = "0px";
+        }  
+        function closeNav() {
+          document.getElementById("sidebar").style.left = "-100vw"; 
+          }
+
+//            function handleMousePos(event) {
+//       var mouseClickWidth = event.clientX;
+//       if(mouseClickWidth>=280){
+//             document.getElementById("mySidebar").style.left = "-100vw";
+//             document.getElementById("sidebar_container_blur_bg").style.right = "-105vw";
+//            }
+// }
+
+// document.addEventListener("click", handleMousePos);
+
+
+
+
+
+
+
+
+
